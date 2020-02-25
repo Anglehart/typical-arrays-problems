8dc42f7f89212953018fa6ce71b13ea3cba39671
Anglehart <planez@bk.ru>
2020-02-25 12:56:14 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  let min = 0;
+  if (Array.isArray(array) && array != []){
+    for (let i = 0; i < array.length; i++){
+      if (min > array[i]){
+        min = array[i];
+      }
+    }
+    return min;
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  let max = 0;
+  if (Array.isArray(array) && array != []){
+    for (let i = 0; i < array.length; i++){
+      if (max < array[i]){
+        max = array[i];
+      }
+    }
+    return max;
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let avg = 0;
+  if (Array.isArray(array) && array.length > 0){
+    for (let i = 0; i < array.length; i++){
+      avg += array[i];
+    }
+    return avg/array.length;
+  } else {
+    return 0;
+  }
 }
